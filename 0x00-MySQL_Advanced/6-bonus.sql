e424daf7a6892eab3c835093467ae6ec1c89cdb5
Cyril-777 <72940048+Cyril-777@users.noreply.github.com>
2023-10-13 03:47:25 +0300

    task 6 - another solution

--- 0x00-MySQL_Advanced/6-bonus.sql
+++ 0x00-MySQL_Advanced/6-bonus.sql
@@ -1,32 +1,17 @@
 -- A SQL script that creates a stored procedure AddBonus that adds a new correction for a student
 DELIMITER //
 CREATE PROCEDURE AddBonus(
-    IN user_id INT,
-    IN project_name VARCHAR(255),
-    IN score INT
-)
+    IN user_id INT, 
+    IN project_name VARCHAR(255), 
+    IN score FLOAT)
 BEGIN
     DECLARE project_id INT;
-    
-    SELECT id INTO project_id
-    FROM projects
-    WHERE name = project_name;
-    
-    IF project_id IS NULL THEN
+    IF (SELECT COUNT(*) FROM projects WHERE name = project_name) = 0
+    THEN
         INSERT INTO projects (name) VALUES (project_name);
-        SET project_id = LAST_INSERT_ID();
     END IF;
-    
-    INSERT INTO corrections (user_id, project_id, score) VALUES (user_id, project_id, score);
-    
-    -- update user's avg score
-    UPDATE users
-    SET average_score = (
-        SELECT AVG(score)
-        FROM corrections
-        WHERE user_id = user_id
-    )
-    WHERE id = user_id;
-END;
+    SET project_id = (SELECT id FROM projects WHERE name = project_name LIMIT 1);
+    INSERT INTO corrections (user_id, project_id, score) VALUES(user_id, project_id, score);
+END
 //
 DELIMITER ;
