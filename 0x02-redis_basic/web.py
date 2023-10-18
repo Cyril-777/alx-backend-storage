f09245bb09a16fe542d71b251adf567a5530b668
Cyril-777 <72940048+Cyril-777@users.noreply.github.com>
2023-10-19 02:17:30 +0300

    adv task solution #2

--- 0x02-redis_basic/web.py
+++ 0x02-redis_basic/web.py
@@ -9,46 +9,40 @@ import requests
 from functools import wraps
 
 
-class CacheWeb:
+r = redis.Redis()
+
+
+def url_access_count(self, method):
     """
-    A class for caching and tracking method calls.
+    A decorator for the get_page function.
     """
+    @wraps(method)
+    def wrapper(url):
+        key = f"cached:{url}"
+        cached_value = r.get(key)
+        if cached_value:
+            return cached_value.decode("utf-8")
 
-    def __init__(self):
-        """ Initialize the Cache object"""
-        self._redis = redis.Redis()
+        key_count = f"count:{url}"
+        html_content = method(url)
 
-    def url_access_count(self, method):
-        """
-        A decorator for the get_page function.
-        """
-        @wraps(method)
-        def wrapper(url):
-            key = f"cached:{url}"
-            cached_value = self._redis.get(key)
-            if cached_value:
-                return cached_value.decode("utf-8")
+        r.incr(key_count)
+        r.set(key, html_content, ex=10)
+        r.expire(key, 10)
+        return html_content
 
-            key_count = f"count:{url}"
-            html_content = method(url)
+    return wrapper
 
-            self._redis.incr(key_count)
-            self._redis.set(key, html_content, ex=10)
-            self._redis.expire(key, 10)
-            return html_content
 
-        return wrapper
-
-    @url_access_count
-    def get_page(self, url: str) -> str:
-        """
-        Obtain the HTML content, track the number of accesses,
-        and cache the result with a 10-second expiration.
-        """
-        results = requests.get(url)
-        return results.text
+@url_access_count
+def get_page(self, url: str) -> str:
+    """
+    Obtain the HTML content, track the number of accesses,
+    and cache the result with a 10-second expiration.
+    """
+    results = requests.get(url)
+    return results.text
 
 
 if __name__ == "__main__":
-    cache_web = CacheWeb()
-    cache_web.get_page('http://slowwly.robertomurray.co.uk')
+    get_page('http://slowwly.robertomurray.co.uk')
