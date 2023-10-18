b76347f363d823b74eca5e738badd86bf67d58a3
Cyril-777 <72940048+Cyril-777@users.noreply.github.com>
2023-10-19 01:27:24 +0300

    callable lib

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -7,6 +7,7 @@
 
 import redis
 import uuid
+from typing import Callable
 import functools
 
 
@@ -15,21 +16,21 @@ class Cache:
         self._redis = redis.Redis()
         self._redis.flushdb()
 
-    def store(self, data) -> str:
+    def store(self, data: str) -> str:
         key = str(uuid.uuid4())
         self._redis.set(key, data)
         return key
 
-    def get(self, key, fn=None):
+    def get(self, key: str, fn: Callable = None):
         data = self._redis.get(key)
         if data is not None and fn is not None:
             return fn(data)
         return data
 
-    def get_str(self, key):
+    def get_str(self, key: str):
         return self.get(key, fn=lambda d: d.decode("utf-8"))
 
-    def get_int(self, key):
+    def get_int(self, key: str):
         return self.get(key, fn=int)
 
 
